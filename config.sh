e01c3d53ee9a2e0691a55fdae6ab08db53052fa1
ShunShun <60352276+Shun-Harutaro@users.noreply.github.com>
2021-03-15 20:35:21 +0900

    Update config.sh
    Vim-Plug for neovim

--- config.sh
+++ config.sh
@@ -1,3 +1,3 @@
 ln -sf ~/.config/nvim/iceberg/colors/iceberg.vim ~/.config/nvim/colors/iceberg.vim
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
