4d2dcaed3831764109c1369ffa7fde620170d969
Shun-Harutaro <n.shunshun2004@gmail.com>
2021-03-15 20:20:30 +0900

    change config.sh

--- config.sh
+++ config.sh
@@ -1,1 +1,3 @@
 ln -sf ~/nvim/iceberg/colors/iceberg.vim ~/nvim/colors/iceberg.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
